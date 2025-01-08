42a44222e8636cec7575c36d33417df86e19a848
0x network <info@0x-network.com>
2025-01-07 22:58:26 -0300

    Create agent.js

---
+++ agent.js
@@ -0,0 +1,48 @@
+// Import the module
+const axios = require('axios');
+
+class AgentQuery{
+    /*
+    * Construct a instance of the Agent
+    */
+   constructor(apiKey, agentId, baseUrl = "https://api.0x-network.com/index.php"){
+    this.apiKey = apiKey;
+    this.agentId = agentId;
+    this.baseUrl = baseUrl;
+   }
+
+   encodeMessage(message){
+    return Buffer.from(message).toString('base64');
+   }
+
+   async queryAgent(message){
+    try{
+        const encodeMessage = this.encodeMessage(message);
+
+        const url = `${this.baseUrl}?action=query-agent&api_key=${this.apiKey}&agent_id=${this.agentId}&message=${encodeMessage}`;
+        const response = await axios.get(url);
+
+        return response.data;
+    }catch(e){
+        console.error("Error: ", e.message);
+    }
+   }
+}
+
+// Let's execute it now
+
+const API_KEY = "OThVNDFvV0p4TGs1aUJXR2dKRWRkNEdyZjVSNDNoRE5KVGJ6Z1VxMTZmeVg=";
+const AGENT_ID = 11;
+// Instanciate the agent
+const agentQuery = new AgentQuery(API_KEY, AGENT_ID);
+
+async function executeQuery(){
+    const message = 'Who are you';
+    const result = await agentQuery.queryAgent(message);
+    console.log("Agent Response: ", result);
+}
+
+// Execute
+(async () => {
+    await executeQuery();
+})();
